19c4c74f96c400c54279454a739cc3040222254f
Kyle Coffey <me@phytolizer.dev>
2023-03-22 18:41:47 -0500

    zign't

--- src/c.zig
+++
@@ -1,5 +0,0 @@
-pub usingnamespace @cImport({
-    @cInclude("SDL.h");
-    @cInclude("cairo/cairo.h");
-    @cInclude("pango/pangocairo.h");
-});
--- src/global.zig
+++
@@ -1,3 +0,0 @@
-const std = @import("std");
-
-pub const allocator = std.heap.c_allocator;
